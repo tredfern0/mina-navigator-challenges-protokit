a04aca3006d62c59f0387cafc7cf4a1c03ac6110
Thomas <thomasr928@gmail.com>
2024-04-30 07:24:17 -0500

    Finish off challenge

--- packages/chain/src/challenge4.ts
+++ packages/chain/src/challenge4.ts
@@ -6,94 +6,45 @@ import { Struct, Field, PublicKey, Bool, Experimental, Poseidon, Nullifier, Merk
 
 export class MessagePublicOutput extends Struct({
   agentId: Field,
-  messageNumber: Field,
-  root: Field,
+  securityCodeHash: Field,
+  messageNumber: UInt64,
 }) { }
 
-export const message: Field[] = [Field(0)];
-
-// create a new tree
-const height = 20;
-const tree = new MerkleTree(height);
-class MerkleWitness20 extends MerkleWitness(height) { }
-
-export function canClaim(
+export function verifyMessage(
   agentId: Field,
-  witness: MerkleWitness20,
-  messageDetails: Field,
+  message: Message,
 ): MessagePublicOutput {
-
-
-  // TODO - 
-  // we need to verify the other data here somehow... 
-
-
-  // how can we check securityId...
-  // need a merkleMap?  Actually yes that would work?
-
-
-  // The security code matches that held for that AgentID
-  assert(agentInfoObj.securityCode.equals(message.securityCode), "Security code does not match!");
 
   // The message is of the correct length - assuming it can't have leading zeros?  Not exactly clear on requirement
   assert(message.twelveChars.greaterThanOrEqual(100000000000), "Twelve chars is not 12 characters long!");
   assert(message.twelveChars.lessThanOrEqual(999999999999), "Twelve chars is not 12 characters long!");
 
-  // The message number is greater than the highest so far for that agent.
-  assert(agentInfoObj.messageNumber.lessThan(message.messageNumber), "Message number too low!");
-
-  // You should update the agent state to store the last message number received.
-  agentInfoObj.messageNumber = message.messageNumber;
-  this.agentMap.set(agentId, agentInfoObj);
-
-
-
-  // get the witness for the current tree
-  // const witness = new MerkleWitness20(tree.getWitness(incrementIndex));
-
-  // Verify witness - proves this is a valid agent
-  const root = witness.calculateRoot(agentId);
-
-
-  //const key = Poseidon.hash(nullifier.getPublicKey().toFields());
-  // const [computedRoot, computedKey] = witness.computeRootAndKey(
-  //   Bool(true).toField()
-  // );
-  // computedKey.assertEquals(key);
-
-  // nullifier.verify(message);
-
-  // Output:
-  // agentId
-  // message
-  // something to verify merkle witness was valid...
+  const securityCodeHash = Poseidon.hash([message.securityCode]);
 
   return new MessagePublicOutput({
     agentId: agentId,
-    messageNumber: messageDetails,
-    root: root,
+    securityCodeHash: securityCodeHash,
+    messageNumber: message.messageNumber,
   });
 }
-
-export const messageZKP = Experimental.ZkProgram({
-  publicOutput: MessagePublicOutput,
-  methods: {
-    canClaim: {
-      privateInputs: [MerkleMapWitness, Nullifier],
-      method: canClaim,
-    },
-  },
-});
-
-export class MessageProof extends Experimental.ZkProgram.Proof(messageZKP) { }
-
 
 // Not exactly clear what 'twelveChars' is, I asked in Slack but never got a response
 // Assuming it's a field that should be exactly twelve decimal characters long with no leading zeroes
 export class Message extends Struct({ agentId: Field, messageNumber: UInt64, twelveChars: Field, securityCode: Field }) { }
 
+export const messageZKP = Experimental.ZkProgram({
+  publicOutput: MessagePublicOutput,
+  methods: {
+    verifyMessage: {
+      privateInputs: [Field, Message],
+      method: verifyMessage,
+    },
+  },
+});
+export class MessageProof extends Experimental.ZkProgram.Proof(messageZKP) { }
+
 // This will be the stored state for each agent
-export class AgentInfo extends Struct({ messageNumber: UInt64, securityCode: Field }) { }
+export class AgentInfo extends Struct({ messageNumber: UInt64, securityCodeHash: Field, blockHeight: UInt64, nonce: UInt64, sender: PublicKey }) { }
 
 @runtimeModule()
 export class AgentMessageStore extends RuntimeModule<unknown> {
@@ -122,23 +73,37 @@ export class AgentMessageStore extends RuntimeModule<unknown> {
   @runtimeMethod()
   public initializeAgent(
     agentId: Field,
-    securityCode: Field,
+    securityCodeHash: Field,
   ): void {
     // Make sure only admin can add new agents
     const currAdmin = this.adminAddr.get();
     assert(currAdmin.isSome, "Admin must exist!");
     assert(this.transaction.sender.value.equals(currAdmin.value), "Only admin can add agents!");
 
-    // Initialize with 0 for the messageNumber
-    const newAgentInfo = new AgentInfo({ messageNumber: UInt64.from(0), securityCode });
+    // Initialize with 0 for all fields
+    const newAgentInfo = new AgentInfo({
+      messageNumber: UInt64.from(0),
+      securityCodeHash: securityCodeHash,
+      blockHeight: UInt64.from(0),
+      nonce: UInt64.from(0),
+      sender: PublicKey.empty()
+    });
     this.agentMap.set(agentId, newAgentInfo);
   }
 
   @runtimeMethod()
-  public processMessage(
-    agentId: Field,
-    message: Message,
-  ): void {
+  public processMessageProof(msgProof: MessageProof) {
+    // Make sure proof was valid
+    msgProof.verify();
+
+    // Now make the update using those inputs
+    const sender: PublicKey = this.transaction.sender.value;
+    const nonce = this.transaction.nonce.value;
+    const blockHeight = this.network.block.height.value;
+
+    const messageNumber = msgProof.publicOutput.messageNumber
+    const agentId = msgProof.publicOutput.agentId
+    const securityCodeHash = msgProof.publicOutput.securityCodeHash
 
     const agentInfo: Option<AgentInfo> = this.agentMap.get(agentId);
     // The agentId exists in the system
@@ -146,36 +111,16 @@ export class AgentMessageStore extends RuntimeModule<unknown> {
 
     const agentInfoObj = new AgentInfo(agentInfo.value);
 
-    // The security code matches that held for that AgentID
-    assert(agentInfoObj.securityCode.equals(message.securityCode), "Security code does not match!");
+    // We need to do two checks here:
+    // First the check here that the messageNumber is higher than previous
+    assert(agentInfoObj.messageNumber.lessThan(UInt64.from(messageNumber)), "Message number too low!");
+    // Second check that the security code hash matches that held for that AgentID
+    assert(agentInfoObj.securityCodeHash.equals(securityCodeHash), "Security code does not match!");
 
-    // The message is of the correct length - assuming it can't have leading zeros?  Not exactly clear on requirement
-    assert(message.twelveChars.greaterThanOrEqual(100000000000), "Twelve chars is not 12 characters long!");
-    assert(message.twelveChars.lessThanOrEqual(999999999999), "Twelve chars is not 12 characters long!");
-
-    // The message number is greater than the highest so far for that agent.
-    assert(agentInfoObj.messageNumber.lessThan(message.messageNumber), "Message number too low!");
-
-    // You should update the agent state to store the last message number received.
-    agentInfoObj.messageNumber = message.messageNumber;
+    agentInfoObj.messageNumber = UInt64.from(messageNumber);
+    agentInfoObj.sender = sender;
+    agentInfoObj.nonce = UInt64.from(nonce);
+    agentInfoObj.blockHeight = UInt64.from(blockHeight);
     this.agentMap.set(agentId, agentInfoObj);
   }
-
-
-  @runtimeMethod()
-  public processNew(msgProof: MessageProof) {
-    msgProof.verify();
-
-    // Make sure it was valid...
-    const root = this.commitment.get();
-    assert(msgProof.publicOutput.root.equals(root.value), "Root does not match!");
-
-    // Now make the update using those inputs
-    const sender: PublicKey = this.transaction.sender.value;
-    const nonce = this.transaction.nonce.value;
-    const blockHeight = this.network.block.height.value;
-
-  }
-
 }
-
